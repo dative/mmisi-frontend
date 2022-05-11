afd2f9a0a4ea32a81309a40948bd79a2d70894e3
Diego Arthur Costa <troiablender@gmail.com>
2022-05-11 12:42:16 -0300

    feat header

--- src/js/app.ts
+++ src/js/app.ts
@@ -1,1 +1,17 @@
-console.log('Hello World!!!');
+const navBtn = document.querySelector("#menu")
+const menuBar = document.querySelector('[role="menubar"]')
+let menuOpen = false
+
+// @ts-ignore
+navBtn.addEventListener('click', () => {
+  if (!menuOpen) {
+    navBtn?.classList.add("is-active") 
+    menuOpen = true
+  }else {
+    navBtn?.classList.remove("is-active")
+    menuOpen = false
+    console.log(menuOpen)
+  }
+  menuBar?.classList.toggle('flex')
+  menuBar?.classList.toggle('hidden')
+})
