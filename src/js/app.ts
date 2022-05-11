24c76ad828fef939d92364e87a62820884c6dd4b
Gabsop <gabsop@hotmail.com>
2022-05-11 16:19:07 -0300

    Typography added and refactor

--- src/js/app.ts
+++ src/js/app.ts
@@ -1,1 +1,10 @@
-console.log('Hello World!!!');
+const button:HTMLElement|null = document.getElementById('hamburger-button');
+button?.addEventListener('click', addClass);
+
+function addClass() {
+  if (button?.classList.contains('is-active')) {
+    button.classList.remove('is-active');
+    return;
+  }
+  button?.classList.add('is-active');
+}
