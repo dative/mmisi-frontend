979cdbd82003810dff1aa6cef07b59eb13fc31c1
Nathan <novadrake76@gmail.com>
2022-05-13 10:34:31 -0300

    fix: sidebar function now in app.ts

--- src/js/app.ts
+++ src/js/app.ts
@@ -1,1 +1,14 @@
-console.log('Hello World!!!');
+const hamburguerBtn = document.getElementById('hamburguer')
+const sideScreen = document.getElementById('sidebar')
+
+if(sideScreen && hamburguerBtn!= null){
+
+hamburguerBtn.addEventListener("click", sidebar)
+
+function sidebar() {
+
+    hamburguerBtn!.classList.toggle("is-active")
+
+  sideScreen!.classList.toggle("hidden")
+  }
+}
