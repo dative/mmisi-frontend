72ac66d5c11ec907651d95daba1daf42adb4ffb2
Rubensaymon <rubensaymon2012@hotmail.com>
2022-05-13 00:24:40 -0300

    envio01

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,48 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    extend: {
+      backgroundImage: {
+        'plum-gradient': 'linear-gradient(180deg, #775AA0 0%, #574275 100%)',
+      },
+      maxWidth:{
+        'narrow': '1100px',
+        'heatherNarrow':'1380px',
+      }
+    },
+
+    screens: {
+      'lsm': '360px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1440px',
+    },
+
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'black': '#19191A',
+      'marigold': '#D5A439',
+      'gray1':'#E5E5E5',
+      'accent':{
+        'DEFAULT': '#C3C2C4',
+        'dark': '#717184',
+      },
+      'plum':{
+        'DEFAULT': '#775AA0',
+        'light': '#F8F7FB',
+        'medium': '#C6BEDF',
+      },
+    },
+
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+
   },
   plugins: [
     require('@tailwindcss/typography'),
